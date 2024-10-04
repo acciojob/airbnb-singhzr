b6a290ed48687fbee1ba7d70c3b01dfe9a80f8ea
Suraj Singh <mesinghzr@gmail.com>
2024-10-04 21:56:34 +0530

    1st Push

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -1,5 +1,6 @@
 package com.driver.controllers;
 
+import com.driver.controllers.HotelManagementService;
 import com.driver.model.Booking;
 import com.driver.model.Facility;
 import com.driver.model.Hotel;
@@ -20,7 +21,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/hotel")
 public class HotelManagementController {
-
+    HotelManagementService hotelManagementService= new HotelManagementService();
     @PostMapping("/add-hotel")
     public String addHotel(@RequestBody Hotel hotel){
 
@@ -28,9 +29,8 @@ public class HotelManagementController {
         //incase the hotelName is null or the hotel Object is null return an empty a FAILURE
         //Incase somebody is trying to add the duplicate hotelName return FAILURE
         //in all other cases return SUCCESS after successfully adding the hotel to the hotelDb.
+        return hotelManagementService.addHotel(hotel);
 
-
-        return null;
     }
 
     @PostMapping("/add-user")
@@ -39,7 +39,7 @@ public class HotelManagementController {
         //You need to add a User Object to the database
         //Assume that user will always be a valid user and return the aadharCardNo of the user
 
-       return null;
+        return hotelManagementService.addUser(user);
     }
 
     @GetMapping("/get-hotel-with-most-facilities")
@@ -49,7 +49,7 @@ public class HotelManagementController {
         //Incase there is a tie return the lexicographically smaller hotelName
         //Incase there is not even a single hotel with atleast 1 facility return "" (empty string)
 
-        return null;
+        return hotelManagementService.getHotelWithMostFacilities();
     }
 
     @PostMapping("/book-a-room")
@@ -59,17 +59,17 @@ public class HotelManagementController {
         //Have bookingId as a random UUID generated String
         //save the booking Entity and keep the bookingId as a primary key
         //Calculate the total amount paid by the person based on no. of rooms booked and price of the room per night.
-        //If there arent enough rooms available in the hotel that we are trying to book return -1 
-        //in other case return total amount paid 
-        
-        return 0;
+        //If there arent enough rooms available in the hotel that we are trying to book return -1
+        //in other case return total amount paid
+
+        return hotelManagementService.bookARoom(booking);
     }
-    
+
     @GetMapping("/get-bookings-by-a-person/{aadharCard}")
     public int getBookings(@PathVariable("aadharCard")Integer aadharCard)
     {
-        //In this function return the bookings done by a person 
-        return 0;
+        //In this function return the bookings done by a person
+        return hotelManagementService.getBookings(aadharCard);
     }
 
     @PutMapping("/update-facilities")
@@ -79,7 +79,7 @@ public class HotelManagementController {
         //If the hotel is already having that facility ignore that facility otherwise add that facility in the hotelDb
         //return the final updated List of facilities and also update that in your hotelDb
         //Note that newFacilities can also have duplicate facilities possible
-        return null;
+        return hotelManagementService.updateFacilities(newFacilities,hotelName);
     }
 
 }
